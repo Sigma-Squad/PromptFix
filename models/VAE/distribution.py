93c34d8b9bf3a69cb4a3f2e1bc0a42deb68cc53d
Shiva9361 <shivadharshansankar936@gmail.com>
2025-02-18 22:03:09 +0530

    fix: annotations

--- models/VAE/distribution.py
+++ models/VAE/distribution.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class DiagonalGaussianDistribution(object):
 
-    def __init__(self, parameter, deterministic=False) -> None:
+    def __init__(self, parameter: torch.Tensor, deterministic: bool = False) -> None:
         self.parameter = parameter
         self.mean, self.logvar = torch.chunk(parameter, 2, dim=-1)
         self.logvar = torch.clamp(self.logvar, -30.0, 20.0)
@@ -16,13 +16,13 @@ class DiagonalGaussianDistribution(object):
             self.var = self.std = torch.zeros_like(
                 self.mean).to(self.parameter.device)
 
-    def sample(self):
+    def sample(self) -> torch.Tensor:
         if self.deterministic:
             return self.mean
         else:
             return self.mean + self.std * torch.randn_like(self.std)
 
-    def kl(self, other=None):
+    def kl(self, other: torch.Tensor = None) -> torch.Tensor:
         if self.deterministic:
             return torch.tensor([0.0])
         else:
@@ -31,10 +31,10 @@ class DiagonalGaussianDistribution(object):
             else:
                 return 0.5 * torch.sum(torch.pow(self.mean - other.mean, 2) / other.var + self.var / other.var - self.logvar + other.logvar - 1, dim=[1, 2, 3])
 
-    def nll(self, sample, dims=[1, 2, 3]):
+    def nll(self, sample: torch.Tensor, dims: list = [1, 2, 3]) -> torch.Tensor:
         if self.deterministic:
             return torch.tensor([0.0])
         return 0.5*torch.sum(torch.pow(sample-self.mean, 2)/self.var + self.logvar + np.log(2.0*np.pi), dim=dims)
 
-    def mode(self):
+    def mode(self) -> torch.Tensor:
         return self.mean
